c19d5965e63e560ba4c7e5a0323ed5ad5be4f0db
chegebaevraatbek <tasabekovraatbek999@gmail.com>
2023-03-02 15:36:56 +0600

    dog

---
+++ dog.js
@@ -0,0 +1,55 @@
+const breeds = document.querySelector(".breeds")
+const breedsImg = document.querySelector(".breed-img")
+const btn = document.querySelector(".search-btn")
+const select = document.querySelector(".select-dog")
+const input = document.querySelector(".search-input")
+
+function fetchAll(){
+    axios.get(`https://dog.ceo/api/breeds/list/all`)
+        .then((res) => {
+            Object.keys(res.data.message).map((el) => {
+                // breeds.innerHTML += `<button class="breed-btn btn btn-primary m-1">${el}</button>`
+            })
+            Object.keys(res.data.message).map((el)=> {
+                select.innerHTML += `<option class="text-white bg-black" value="${el}">${el}</option>`
+            })
+        })
+        .then(() => getBtn())
+}
+select.addEventListener("change", (e) => {
+     fetchImg(e.target.value)
+})
+
+btn.addEventListener("click",() => {
+    fetchImg(`${input.value}`)
+})
+
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter"){
+        fetchImg(e.target.value)
+    }
+})
+
+input.addEventListener("input", (e)=> {
+    fetchImg(e.target.value)
+})
+
+fetchAll()
+
+function getBtn(){
+    const buttons = document.querySelectorAll(".breed-btn")
+    buttons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            fetchImg(btn.innerHTML)
+        })
+    })
+}
+function fetchImg(API){
+    axios(`https://dog.ceo/api/breed/${API}/images/random`)
+        .then((res) => {
+            breedsImg.innerHTML = `<img src="${res.data.message}" alt="" class="card" style="width: auto;margin: 0 auto">`
+        })
+}
+
+
+
